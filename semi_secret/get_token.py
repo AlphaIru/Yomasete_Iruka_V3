8498aff0447e2b3a53e7e9f3188c70c3f6ba3c42
Tom_XV <55909638+TomXV@users.noreply.github.com>
2022-10-06 20:25:21 +0900

    Update get_token.py

--- semi_secret/get_token.py
+++ semi_secret/get_token.py
@@ -1,12 +1,13 @@
 """トークンを取得するコード."""
 import json
 
-TOKEN_DIRECTORY = "/home/ubuntu/tokens.json"  # nosec
+
+# TOKEN_DIRECTORY = "/home/ubuntu/tokens.json"  # nosec
 
 
 def get_token(bot_name):
     """トークンを取得する."""
-    with open(TOKEN_DIRECTORY, mode="r", encoding="utf-8") as file:
+    with open("tokens.json", mode="r", encoding="utf-8") as file:
         token_dictionary = json.load(file)
 
     token_key = token_dictionary[bot_name]
